f71f0b78217a3a20b60da8a2e82516233796539c
Max Taylor <maxbransontaylor@gmail.com>
2022-10-11 00:35:04 -0400

    didnt do lesson 3 tests

---
+++ src/components/About/__tests__/index.test.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { render, cleanup } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+import About from "..";
+afterEach(cleanup);
+
+describe("About components", () => {
+  it("renders", () => {
+    render(<About />);
+  });
+  it("matches snapshot DOM node structure", () => {
+    // render About
+    const { asFragment } = render(<About />);
+    expect(asFragment()).toMatchSnapshot();
+  });
+});
